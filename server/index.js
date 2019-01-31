9dba58aaabbf1595c789bd9c60de09370640231e
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2019-01-31 22:15:57 +0100

    chore: 2 space indent

--- server/index.js
+++ server/index.js
@@ -5,28 +5,31 @@ const Manifest = require('./manifest');
 
 exports.deployment = async (start) => {
 
-    const manifest = Manifest.get('/');
-    const server = await Glue.compose(manifest, { relativeTo: __dirname });
+  const manifest = Manifest.get('/');
+  const server = await Glue.compose(manifest, { relativeTo: __dirname });
 
-    await server.initialize();
+  await server.initialize();
 
-    if (!start) {
-        return server;
-    }
-
-    await server.start();
-
-    console.log(`Server started at ${server.info.uri}`);
-
+  if (!start) {
     return server;
+  }
+
+  await server.start();
+
+  console.log(`Server started at ${server.info.uri}`);
+
+  server.subscription('/topic/{topic}');
+  server.broadcast('Hello, welcome!');
+
+  return server;
 };
 
 if (!module.parent) {
 
-    exports.deployment(true);
+  exports.deployment(true);
 
-    process.on('unhandledRejection', (err) => {
+  process.on('unhandledRejection', (err) => {
 
-        throw err;
-    });
+    throw err;
+  });
 }
