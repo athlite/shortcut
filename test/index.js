3636f317e14a08e961a4cfcc9161c79872f4024f
Thomas <thomas.eng@drivenordic.com>
2019-02-04 11:37:18 +0100

    test(route-create-topic): add spec

--- test/index.js
+++ test/index.js
@@ -4,20 +4,39 @@
 
 const Code = require('code');
 const Lab = require('lab');
+const Nes = require('nes');
 const Server = require('../server');
 const Package = require('../package.json');
 
 // Test shortcuts
-
-const { describe, it } = exports.lab = Lab.script();
+const { describe, it, before } = exports.lab = Lab.script();
 const { expect } = Code;
 
-describe('Deployment', () => {
+describe('Hapi', () => {
+  describe('Deployment', () => {
 
-  it('registers the main plugin.', async () => {
+    it('registers the main plugin.', async () => {
 
-    const server = await Server.deployment();
+      const server = await Server.deployment();
 
-    expect(server.registrations[Package.name]).to.exist();
+      expect(server.registrations[Package.name]).to.exist();
+    });
+  });
+
+  describe('Routes', () => {
+
+    let server;
+
+    before(async () => {
+      server = await Server.deployment();
+    }); 
+
+    it('should create new topic on javascript', async () => {
+      const response = await server.inject({
+        method: 'get',
+        url: '/create-topic/javascript'
+      });
+      expect(response.result.message).to.equal('Created topic javascript. To subscribe use endpoint /topic/javascript');
+    });
   });
 });
