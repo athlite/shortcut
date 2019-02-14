19f63de229ffed67b928218493dc6f957b2dc258
Thomas <thomas.eng@drivenordic.com>
2019-02-14 15:34:15 +0100

    chore(webpack-config): remove cacheGroups

--- webpack.config.js
+++ webpack.config.js
@@ -10,20 +10,7 @@ const config = {
   },
   optimization: {
     splitChunks: {
-      chunks: 'all',
-      /*cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            // get the name. E.g. node_modules/packageName/not/this/part.js
-            // or node_modules/packageName
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-
-            // npm package names are URL-safe, but some servers don't like @ symbols
-            return `npm.${packageName.replace('@', '')}`;
-          },
-        },
-      },*/
+      chunks: 'all'
     },
   },
   module: {
