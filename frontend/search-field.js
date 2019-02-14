df94f25a4ed221b5f35a19945fdac61ccc372953
Thomas <thomas.eng@drivenordic.com>
2019-02-14 15:32:56 +0100

    feat(lost): add postcss-lost

--- frontend/search-field.js
+++ frontend/search-field.js
@@ -1,19 +1,24 @@
 import React from 'react';
+import './search-field.css';
 
 export default ({ onchange, onenter, value }) => (
-  <div className="field">
-    <label htmlFor="search">Search:</label>
-    <input
-      onChange={e => onchange(e.target.value)}
-      onKeyUp={e => {
-        if (e.key !== 'Enter') {
-          return;
-        }
-        onenter(e.target.value);
-      }}
-      value={value}
-      id="search"
-      placeholder="for something ..."
-      autoComplete="off" />
+  <div className="field gridify">
+    <div className="label">
+      <label htmlFor="search">Search:</label>
+    </div>
+    <div className="control">
+      <input
+        onChange={e => onchange(e.target.value)}
+        onKeyUp={e => {
+          if (e.key !== 'Enter') {
+            return;
+          }
+          onenter(e.target.value);
+        }}
+        value={value}
+        id="search"
+        placeholder="for something ..."
+        autoComplete="off" />
+    </div>
   </div>
 );
--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,17 @@
-module.exports = {
+
+const config = {
   plugins: {
     'postcss-import': {},
-    'postcss-preset-env': {},
-    'cssnano': {}
+    'postcss-preset-env': {
+      stage: 0,
+      preserve: false
+    },
+    'lost': {}
   }
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins['cssnano'] = {};
 }
+
+module.exports = config;
